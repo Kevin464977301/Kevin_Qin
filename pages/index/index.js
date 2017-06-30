528999fae61c74fe7d29baa636f3871aeaf2cdf6
秦凯华 <464977301@qq.com>
2017-06-30 16:09:07 +0800

    让地图显示在页面上

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,8 @@
 //index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    //两个键值中需要用逗号(,)进行区分
+    a: 18
+   //b: "嘻嘻哈哈"
   }
 })
